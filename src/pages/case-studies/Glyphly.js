99067beef6b308acbc673f75ebe73bc510c4a989
Therese <twa140@sfu.ca>
2026-05-28 12:57:35 -0700

    minor fix

--- src/pages/case-studies/Glyphly.js
+++ src/pages/case-studies/Glyphly.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 
 import { projectData } from "../../data/projectData";
 import CaseStudyLayout from "../../pages/CaseStudy.js";
