1dac6ed5ab09b0c5f0a0047ea31b303c034a5bcc
Therese <twa140@sfu.ca>
2026-06-02 15:10:19 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-// Import HashRouter instead of BrowserRouter
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* Wrap your App component in HashRouter */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
